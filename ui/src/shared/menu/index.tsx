aa15403319715c8a2e1fb9806060e1804e7a9550
Roy Mosby <ego.madking@gmail.com>
2023-01-08 10:39:28 -0500

    create initial links for menu

--- ui/src/shared/menu/index.tsx
+++ ui/src/shared/menu/index.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export function Menu() {
   return (
     <nav>
-      <h2>Menu</h2>
+      <Link to='/'>All Recipes</Link>
+      <Link to='book'>Your Own Recipes</Link>
+      <Link to='login'>Login</Link>
+      <Link to='/'>New Recipe</Link>
+      <Link to='/'>Profile</Link>
+      <Link to='/'>Recipe</Link>
+      <Link to='/'>Dev</Link>
     </nav>
   );
 }
