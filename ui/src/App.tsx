c714233bbbea6cc7ab3fb6b6b2d8d2cbf5af16ed
Roy Mosby <ego.madking@gmail.com>
2023-01-08 10:37:26 -0500

    remove header, footer

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,14 +2,11 @@ import { RouterProvider } from 'react-router-dom';
 
 import './App.scss';
 import { router } from './routes';
-import { Header, Footer } from './shared';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <RouterProvider router={router}></RouterProvider>
-      <Footer />
+      <RouterProvider router={router} />
     </div>
   );
 }
